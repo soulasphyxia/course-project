d82f5258f660586d423bc819d1b4eeaee206793b
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-10-03 17:47:55 +0700

    initial commit

--- src/main/java/org/soulasphyxia/webcourseproject/WebCourseProjectApplication.java
+++ src/main/java/org/soulasphyxia/webcourseproject/WebCourseProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebCourseProjectApplication {
 
     public static void main(String[] args) {
